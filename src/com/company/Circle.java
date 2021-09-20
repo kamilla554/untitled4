a3ef405761fe1679de77b6f88d9d75d8db113601
User <khayrullina.k.i@edu.mirea.ru>
2021-09-20 16:31:20 +0300

    готово

---
+++ src/com/company/Circle.java
@@ -0,0 +1,44 @@
+package com.company;
+
+public class Circle extends Shape {
+    protected double radius;
+
+    public Circle() {
+    }
+
+    public Circle(double radius) {
+        this.radius = radius;
+    }
+
+    public Circle(String color, boolean filled, double radius) {
+        super(color, filled);
+        this.radius = radius;
+    }
+
+    public double getRadius() {
+        return radius;
+    }
+
+    public void setRadius(double radius) {
+        this.radius = radius;
+    }
+
+    @Override
+    public double getArea() {
+        return radius*radius*Math.PI;
+    }
+
+    @Override
+    public double getPerimeter() {
+        return 2*Math.PI*radius;
+    }
+
+    @Override
+    public String toString() {
+        return "Circle{" +
+                "radius=" + radius +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
---
+++ src/com/company/Square.java
@@ -0,0 +1,43 @@
+package com.company;
+
+public class Square extends Rectangle {
+    private double side;
+    public Square() {
+    }
+
+    public Square(double side) {
+        this.side=side;
+    }
+
+    public Square(double width, double length, double side) {
+        super(width, length);
+        this.side = side;
+    }
+
+    public double getSide() {
+        return side;
+    }
+
+    public void setSide(double side) {
+        this.side = side;
+    }
+
+    @Override
+    public void setWidth(double side) {
+        super.setWidth(side);
+    }
+
+    @Override
+    public void setLength(double side) {
+        super.setLength(side);
+    }
+
+    @Override
+    public String toString() {
+        return "Square{" +
+                "side=" + side +
+                ", color='" + color + '\'' +
+                ", filled=" + filled +
+                '}';
+    }
+}
